9f31276b5e2369798a2b3b23fbc679cf6457b011
Hasibul Hasan Shanto <55630679+Shantovai23@users.noreply.github.com>
2019-10-03 19:04:00 +0600

    C

---
+++ 1050.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+
+
+    scanf("%d",&n);
+
+    if(n==61)
+    {
+        printf("Brasilia\n");
+
+    }
+    else if(n==71)
+    {
+        printf("Salvador\n");
+
+    }
+    else if(n==11)
+    {
+        printf("Sao Paulo\n");
+
+    }
+   else  if(n==21)
+    {
+        printf("Rio de Janeiro\n");
+
+    }
+    else if(n==32)
+    {
+        printf("Juiz de Fora\n");
+
+    }
+    else if(n==19)
+    {
+        printf("Campinas\n");
+
+    }
+    else if(n==27)
+    {
+        printf("Vitoria\n");
+
+    }
+    else if(n==31)
+    {
+        printf("Belo Horizonte\n");
+
+    }
+    else if(n!=61 || n!=71 || n!=11 || n!=21 || n!=32 || n!=19 || n!=27 || n!=31)
+    {
+        printf("DDD nao cadastrado\n");
+    }
+    return 0;
+
+
+
+
+
+
+
+
+}
