223d12d8d85c45e499594923164a5b740c1ada58
Hasibul Hasan Shanto <55630679+Shantovai23@users.noreply.github.com>
2020-08-17 20:09:28 +0600

    C

---
+++ 1096.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    int I,J,a=7,b=6,c=5;
+
+    for(I=1; I<=9; I+=2){
+        printf("I=%d J=%d\n",I,a);
+         printf("I=%d J=%d\n",I,b);
+          printf("I=%d J=%d\n",I,c);
+    }
+
+return 0;
+}
