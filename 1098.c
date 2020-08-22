b62c46db6054519af2f055b6314831de795aa6e7
Hasibul Hasan Shanto <55630679+Shantovai23@users.noreply.github.com>
2020-08-22 22:03:03 +0600

    C

---
+++ 1098.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main(){
+
+    float I,J,a=1,b=2,c=3;
+
+    for(I=0; I<=2.2; I+=0.2){
+       printf("%f\n",I);
+        printf("%f\n",I);
+         printf("%f\n",I);
+    }
+
+return 0;
+}
+
